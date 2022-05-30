4b906b50adf23db2429af8d14e34a67c46e63ed8
Watchanon Numnam <jiramot@gmail.com>
2022-05-30 12:24:28 +0700

    Init

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	fmt.Println("start")
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+	_ = rdb.FlushDB(ctx).Err()
+
+	var incrLimit = redis.NewScript(`
+		local key = KEYS[1]
+		local limit = tonumber(ARGV[1])
+		redis.log(redis.LOG_NOTICE, "key", key, "limit", limit)
+		local value = redis.call("incr", key)
+		if( value > limit )
+		then
+			return redis.error_reply('over limit')
+		end
+		return value
+	`)
+	keys := []string{"hello"}
+	limit := []interface{}{10}
+
+	for i := 1; i < 20; i++ {
+		fmt.Printf("loop %d\n", i)
+		num, err := incrLimit.Run(ctx, rdb, keys, limit).Int()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(num)
+	}
+}
